3c5eed287c3f98316e7516f26b17e22f38e4c318
Viktor Pakhai <vpakhai@softserveinc.com>
2023-08-09 13:18:31 +0300

    Add volums to deployment

--- start.sh
+++ start.sh
@@ -3,3 +3,9 @@
 #create cluster
 eksctl create cluster -f eks-cluster.yaml --profile gpt
 
+sleep 30s
+
+# install Metrics Server in Kubernetes cluster
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+sleep 5s
+kubectl apply -f ./k8s-config/.
