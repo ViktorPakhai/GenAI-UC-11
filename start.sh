f57c262e03adac21b527cc6a109562357af5c144
Viktor Pakhai <vpakhai@softserveinc.com>
2023-08-09 14:16:30 +0300

    Added PDB

--- start.sh
+++ start.sh
@@ -5,7 +5,9 @@ eksctl create cluster -f eks-cluster.yaml --profile gpt
 
 sleep 30s
 
-# install Metrics Server in Kubernetes cluster
+# install Metrics Server in Kubernetes cluster, needed for correct work horizontal pod autoscaller
+
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
 sleep 5s
 kubectl apply -f ./k8s-config/.
